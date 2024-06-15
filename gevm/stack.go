72f5d7f0135d167aac209b68910eb74758a1be02
Jesserc <raymondjesse713@gmail.com>
2024-06-15 20:07:46 +0100

    updated stack📚

--- gevm/stack.go
+++ gevm/stack.go
@@ -3,6 +3,8 @@ package gevm
 import (
 	"errors"
 	"fmt"
+
+	"github.com/holiman/uint256"
 )
 
 var (
@@ -13,34 +15,34 @@ var (
 const MAX_STACK_SIZE = 1024
 
 type Stack struct {
-	data []byte
+	data []*uint256.Int
 }
 
-func (st *Stack) Push(value []byte) {
+func (st *Stack) Push(value *uint256.Int) {
 	if len(st.data) == MAX_STACK_SIZE {
 		panic(ErrStackOverflow.Error())
 	}
-	st.data = append(st.data, value...)
+	st.data = append(st.data, value)
 }
 
-func (st *Stack) Pop() []byte {
+func (st *Stack) Pop() *uint256.Int {
 	if len(st.data) == 0 {
 		panic(ErrStackUnderflow.Error())
 	}
 	element := st.data[len(st.data)-1]
 	st.data = (st.data)[:len(st.data)-1]
-	return []byte{element}
+	return element
 }
 
 func (st Stack) String() string {
 	var d string
 	for i := len(st.data) - 1; i >= 0; i-- {
 		if i == len(st.data)-1 {
-			d += fmt.Sprintf("%v <first\n", st.data[i])
+			d += fmt.Sprintf("%v <first\n", st.data[i].String())
 		} else if i == 0 {
-			d += fmt.Sprintf("%v <last", st.data[i])
+			d += fmt.Sprintf("%v <last", st.data[i].String())
 		} else {
-			d += fmt.Sprintf("%v\n", st.data[i])
+			d += fmt.Sprintf("%v\n", st.data[i].String())
 		}
 	}
 	return d
