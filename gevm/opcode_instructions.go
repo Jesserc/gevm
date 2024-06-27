c1da2457d8dee1eb9537735b7e90a9e6f1aef0f9
Jesserc <raymondjesse713@gmail.com>
2024-06-27 19:20:42 +0100

    feat: completed instruction set implementation for planned environment opcodes

--- gevm/opcode_instructions.go
+++ gevm/opcode_instructions.go
@@ -13,49 +13,49 @@ func stop(evm *EVM) {
 func add(evm *EVM) {
 	a, b := evm.Stack.Pop(), evm.Stack.Pop()
 	evm.Stack.Push(new(uint256.Int).Add(&a, &b)) // a + b
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(3)
 }
 
 func sub(evm *EVM) {
 	a, b := evm.Stack.Pop(), evm.Stack.Pop()
 	evm.Stack.Push(new(uint256.Int).Sub(&a, &b)) // a - b
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(3)
 }
 
 func mul(evm *EVM) {
 	a, b := evm.Stack.Pop(), evm.Stack.Pop()
 	evm.Stack.Push(new(uint256.Int).Mul(&a, &b)) // a * b
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(3)
 }
 
 func div(evm *EVM) {
 	a, b := evm.Stack.Pop(), evm.Stack.Pop()
 	evm.Stack.Push(new(uint256.Int).Div(&a, &b)) // a / b, returns 0 if b == 0
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(5)
 }
 
 func sdiv(evm *EVM) {
 	a, b := evm.Stack.Pop(), evm.Stack.Pop()
 	evm.Stack.Push(new(uint256.Int).SDiv(&a, &b)) // signed a / b, returns 0 if b == 0
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(5)
 }
 
 func mod(evm *EVM) {
 	a, b := evm.Stack.Pop(), evm.Stack.Pop()
 	evm.Stack.Push(new(uint256.Int).Mod(&a, &b)) // a % b, returns 0 if b == 0
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(5)
 }
 
 func smod(evm *EVM) {
 	a, b := evm.Stack.Pop(), evm.Stack.Pop()
 	evm.Stack.Push(new(uint256.Int).SMod(&a, &b)) // signed a % b, returns 0 if b == 0
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(5)
 }
 
@@ -63,7 +63,7 @@ func addmod(evm *EVM) {
 	a, b := evm.Stack.Pop(), evm.Stack.Pop()
 	mod := evm.Stack.Pop()
 	evm.Stack.Push(new(uint256.Int).AddMod(&a, &b, &mod)) // a + b % mod, returns 0 if mod == 0
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(8)
 }
 
@@ -71,14 +71,14 @@ func mulmod(evm *EVM) {
 	a, b := evm.Stack.Pop(), evm.Stack.Pop()
 	mod := evm.Stack.Pop()
 	evm.Stack.Push(new(uint256.Int).MulMod(&a, &b, &mod)) // a * b % mod, returns 0 if mod == 0
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(8)
 }
 
 func exp(evm *EVM) {
 	a, exponent := evm.Stack.Pop(), evm.Stack.Pop()
 	evm.Stack.Push(new(uint256.Int).Exp(&a, &exponent)) // a ^ exponent
-	evm.PC += 1
+	evm.PC++
 	// gas to decrement = 10 + (50 * size_in_bytes(exponent)))
 	evm.gasDec(10 + (50 * uint64(exponent.ByteLen())))
 }
@@ -86,7 +86,7 @@ func exp(evm *EVM) {
 func signextend(evm *EVM) {
 	b, num := evm.Stack.Pop(), evm.Stack.Pop()
 	evm.Stack.Push(new(uint256.Int).ExtendSign(&num, &b))
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(5)
 }
 
@@ -98,7 +98,7 @@ func lt(evm *EVM) {
 		ret = uint256.NewInt(1)
 	}
 	evm.Stack.Push(ret)
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(3)
 }
 
@@ -109,7 +109,7 @@ func slt(evm *EVM) {
 		ret = uint256.NewInt(1)
 	}
 	evm.Stack.Push(ret)
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(3)
 }
 
@@ -120,7 +120,7 @@ func gt(evm *EVM) {
 		ret = uint256.NewInt(1)
 	}
 	evm.Stack.Push(ret)
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(3)
 }
 
@@ -131,7 +131,7 @@ func sgt(evm *EVM) {
 		ret = uint256.NewInt(1)
 	}
 	evm.Stack.Push(ret)
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(3)
 }
 
@@ -142,7 +142,7 @@ func eq(evm *EVM) {
 		ret = uint256.NewInt(1)
 	}
 	evm.Stack.Push(ret)
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(3)
 }
 
@@ -153,7 +153,7 @@ func iszero(evm *EVM) {
 		ret = uint256.NewInt(1)
 	}
 	evm.Stack.Push(ret)
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(3)
 }
 
@@ -161,56 +161,56 @@ func iszero(evm *EVM) {
 func and(evm *EVM) {
 	a, b := evm.Stack.Pop(), evm.Stack.Pop()
 	evm.Stack.Push(new(uint256.Int).And(&a, &b))
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(3)
 }
 
 func or(evm *EVM) {
 	a, b := evm.Stack.Pop(), evm.Stack.Pop()
 	evm.Stack.Push(new(uint256.Int).Or(&a, &b))
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(3)
 }
 
 func xor(evm *EVM) {
 	a, b := evm.Stack.Pop(), evm.Stack.Pop()
 	evm.Stack.Push(new(uint256.Int).Xor(&a, &b))
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(3)
 }
 
 func not(evm *EVM) {
 	a := evm.Stack.Pop()
 	evm.Stack.Push(new(uint256.Int).Not(&a))
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(3)
 }
 
 func _byte(evm *EVM) {
 	i, x := evm.Stack.Pop(), evm.Stack.Pop()
 	evm.Stack.Push(x.Byte(&i))
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(3)
 }
 
 func shl(evm *EVM) {
 	shift, value := evm.Stack.Pop(), evm.Stack.Pop()
 	evm.Stack.Push(new(uint256.Int).Lsh(&value, uint(shift.Uint64())))
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(3)
 }
 
 func shr(evm *EVM) {
 	shift, value := evm.Stack.Pop(), evm.Stack.Pop()
 	evm.Stack.Push(new(uint256.Int).Rsh(&value, uint(shift.Uint64())))
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(3)
 }
 
 func sar(evm *EVM) {
 	shift, value := evm.Stack.Pop(), evm.Stack.Pop()
 	evm.Stack.Push(new(uint256.Int).SRsh(&value, uint(shift.Uint64())))
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(3)
 }
 
@@ -221,7 +221,7 @@ func keccak256(evm *EVM) {
 	value := evm.Memory.Access(offset.Uint64(), size.Uint64())
 	hash := crypto.Keccak256(value)
 	evm.Stack.Push(uint256.NewInt(0).SetBytes(hash))
-	evm.PC += 1
+	evm.PC++
 
 	// Gas cost calculations
 	currentMemSize := uint64(evm.Memory.Len())
@@ -245,7 +245,7 @@ func keccak256(evm *EVM) {
 // Ethereum environment opcodes
 func address(evm *EVM) {
 	evm.Stack.Push(uint256.MustFromHex(evm.Sender.Hex()))
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(3)
 }
 
@@ -255,7 +255,7 @@ func address(evm *EVM) {
 func balance(evm *EVM) {
 	_ = evm.Stack.Pop() // Pop the address from the stack, though it's not used here
 	evm.Stack.Push(uint256.MustFromDecimal("99999999999"))
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(2600) // '2600' here represents "address access cost" and can be 100 for warm access, but we are assuming cold access since this is a mocked version.
 }
 
@@ -265,7 +265,7 @@ func balance(evm *EVM) {
 func origin(evm *EVM) {
 	// We're using evm.Sender because this is a mocked version, but evm.sender may not always be the same as tx.origin in real world cases.
 	evm.Stack.Push(uint256.MustFromHex(evm.Sender.Hex()))
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(2)
 }
 
@@ -274,13 +274,13 @@ func origin(evm *EVM) {
 // caller pushes a mocked address of the current transaction caller in a transaction call chain.
 func caller(evm *EVM) {
 	evm.Stack.Push(uint256.MustFromHex("0xBc73e0231621D6274671839f9dF8EE7E2C8A6f93"))
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(2)
 }
 
 func callvalue(evm *EVM) {
 	evm.Stack.Push(evm.Value)
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(2)
 }
 
@@ -298,7 +298,7 @@ func calldataload(evm *EVM) {
 func calldatasize(evm *EVM) {
 	calldatasize := uint64(len(evm.Calldata))
 	evm.Stack.Push(uint256.NewInt(calldatasize))
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(2)
 }
 
@@ -316,14 +316,14 @@ func calldatacopy(evm *EVM) {
 	staticGas := uint64(3)
 	dynamicGas := staticGas*minWordSize + memExpansionCost
 
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(dynamicGas)
 }
 
 func codesize(evm *EVM) {
 	codesize := uint64(len(evm.Program))
 	evm.Stack.Push(uint256.NewInt(0).SetUint64(codesize))
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(2)
 }
 
@@ -341,7 +341,7 @@ func codecopy(evm *EVM) {
 	staticGas := uint64(3)
 	dynamicGas := staticGas*minWordSize + memExpansionCost
 
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(dynamicGas)
 }
 
@@ -350,34 +350,89 @@ func codecopy(evm *EVM) {
 // gasprice pushes a mocked gas price (0) onto the stack.
 func gasprice(evm *EVM) {
 	evm.Stack.Push(uint256.NewInt(0)) // push 0x0
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(2)
 }
 
 // This is a mocked version and doesn't behave exactly as it would in a real EVM.
 //
-// extcodesize pushes a mocked codesize of an external account (0) onto the stack.
+// extcodesize pushes a mocked code size (0) of an external account onto the stack.
 func extcodesize(evm *EVM) {
 	_ = evm.Stack.Pop()               // Pop external address off the stack
 	evm.Stack.Push(uint256.NewInt(0)) // push 0x0
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(2)
 }
 
+// This is a mocked version and doesn't behave exactly as it would in a real EVM.
+//
+// extcodecopy copies a zero-length mocked byte slice to memory.
 func extcodecopy(evm *EVM) {
 	_ = evm.Stack.Pop() // Pop external address off the stack
 	destMemOffsetU256 := evm.Stack.Pop()
-	_ = evm.Stack.Pop() // Pop offset in the external code to copy.
+	_ = evm.Stack.Pop() // Pop offset of the external code to copy. This isn't used.
 	sizeU256 := evm.Stack.Pop()
 
 	destMemOffset, size := destMemOffsetU256.Uint64(), sizeU256.Uint64()
 
-	extCodeCopy := []byte{} // mocked (no external code)
-	memExpansionCost := evm.Memory.Store(destMemOffset, extCodeCopy)
+	extCodeCopy := []byte{}                                          // mocked (no external code)
+	memExpansionCost := evm.Memory.Store(destMemOffset, extCodeCopy) // extCode has zero length
 
 	minWordSize := toWordSize(size)
 	dynamicGas := 3*minWordSize + memExpansionCost + 2600 // '2600' here represents "address access cost" and can be 100 for warm access, but we are assuming cold access since this is a mocked version.
 
-	evm.PC += 1
+	evm.PC++
 	evm.gasDec(dynamicGas)
+}
+
+// This is a mocked version and doesn't behave exactly as it would in a real EVM.
+//
+// returndatasize pushes a mocked (zero-value) length of the return data onto the stack.
+func returndatasize(evm *EVM) {
+	evm.Stack.Push(uint256.NewInt(0))
+	evm.PC++
+	evm.gasDec(2)
+}
+
+// This is a mocked version and doesn't behave exactly as it would in a real EVM.
+//
+// returndatacopy copies a zero-length mocked return data byte slice to memory
+func returndatacopy(evm *EVM) {
+	destMemOffsetU256 := evm.Stack.Pop()
+	_ = evm.Stack.Pop() // Pop offset of the return data to copy. This isn't used.
+	sizeU256 := evm.Stack.Pop()
+
+	destMemOffset, size := destMemOffsetU256.Uint64(), sizeU256.Uint64()
+
+	retDataCopy := []byte{}
+	memExpansionCost := evm.Memory.Store(destMemOffset, retDataCopy)
+
+	minWordSize := toWordSize(size)
+	staticGas := uint64(3)
+	dynamicGas := staticGas*minWordSize + memExpansionCost
+
+	evm.PC++
+	evm.gasDec(dynamicGas)
+}
+
+// This is a mocked version and doesn't behave exactly as it would in a real EVM.
+//
+// blockhash pushes the hash (mocked hash) of one of the 256 most recent complete blocks onto the stack.
+func blockhash(evm *EVM) {
+	blockNumU256 := evm.Stack.Pop()
+	if blockNumU256.Uint64() > 256 {
+		panic("Only the last 256 blocks can be accessed")
+	}
+	evm.Stack.Push(uint256.MustFromHex("0x29045A592007D0C246EF02C2223570DA9522D0CF0F73282C79A1BC8F0BB2C238")) // push a mocked block hash
+	evm.PC++
+	evm.gasDec(20)
+}
+
+// This is a mocked version and doesn't behave exactly as it would in a real EVM.
+//
+// coinbase pushes a block’s beneficiary address onto the stack.
+func coinbase(evm *EVM) {
+	evm.Stack.Push(uint256.MustFromHex("0x29045A592007D0C246EF02C2223570DA9522D0CF0F73282C79A1BC8F0BB2C238")) // push a mocked coinbase address
+	evm.PC++
+	evm.gasDec(2)
 }
