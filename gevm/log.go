491f87316bb7d5e0d4b3637ff7ad0c6158470085
Jesserc <raymondjesse713@gmail.com>
2024-07-08 22:51:16 +0100

    implement stringer interface for evm log record

--- gevm/log.go
+++ gevm/log.go
@@ -1,6 +1,11 @@
 package gevm
 
-import "github.com/ethereum/go-ethereum/common"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/common"
+)
 
 //lint:ignore U1000 ignore unused fields
 type Log struct {
@@ -12,6 +17,19 @@ type LogRecord []Log
 
 func (l *LogRecord) AddLog(topics []common.Hash, data []byte) {
 	*l = append(*l, Log{topics: topics, data: data})
+}
+
+func (l *LogRecord) String() string {
+	var sb strings.Builder
+	for i, log := range *l {
+		sb.WriteString(fmt.Sprintf("Log %d:\n", i))
+		sb.WriteString("  Topics:\n")
+		for j, topic := range log.topics {
+			sb.WriteString(fmt.Sprintf("    Topic %d: %s\n", j, topic.Hex()))
+		}
+		sb.WriteString(fmt.Sprintf("  Data: %x\n", log.data))
+	}
+	return sb.String()
 }
 
 func NewLogRecord() *LogRecord {
